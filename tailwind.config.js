7051d070e0b751cd4faa6a32a85036f75ef92a1a
Nathanael Roviery <roviery@gmail.com>
2022-02-12 01:21:22 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': "'Poppins', sans-serif"
+      },
+      colors: {
+        'royal': '#111E6C',
+        'azure': '#0800FF',
+        'maya': '#73C2FB',
+        'rov-purple': '#543884',
+        'rov-light-purple': '#9A77CF',
+        'rov-white': '#FCFCFC'
+      }
+    },
+  },
+  plugins: [],
+}
+
+/* 
+  sm -> 640px
+  md -> 768px
+  lg -> 1024px
+  xl -> 1280px
+  2xl -> 1536px
+
+
+*/
